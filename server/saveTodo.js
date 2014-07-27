8267637a2c2b8e7aefcafa0f59040a5ac7fd96a9
Jakowicz <simon@jakowicz.com>
2014-07-27 14:56:43 +0100

    removed absoluet path to json file on the server

--- server/saveTodo.js
+++ server/saveTodo.js
@@ -4,7 +4,7 @@ var express    = require("express");
 var bodyParser = require('body-parser');
 
 // Config
-var filePath = "/Users/simon/sites/Yeoman-React-Bootstrap-Require/server/json/todo.json";
+var filePath = "json/todo.json";
 
 // Start Express app
 var app      = express();
